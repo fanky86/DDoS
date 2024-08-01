48ff092236c977ea6bfd2d460437dfd8cc7839e6
Tanpa Nama <106883384+Viper404-XD@users.noreply.github.com>
2024-08-02 01:42:28 +0700

    Update banner.py

--- script/banner.py
+++ script/banner.py
@@ -1,1 +1,12 @@
 
+
+
+def title():
+  Console().print(Panel("""
+  [bold red]███████████████████████    
+  [bold red]███████████████████████ [bold yellow]Github : [bold green]
+  [bold red]███████████████████████ [bold yellow]Wa     : [bold green]+62895386194***
+  [bold white]███████████████████████          
+  [bold white]███████████████████████          
+  [bold white]███████████████████████ 
+  [bold white]""",width=60,style=f"bold cyan"))
