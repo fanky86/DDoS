c82dc6d0f16d1f513eef66d4105962f3f4e94b24
Tanpa Nama <106883384+Viper404-XD@users.noreply.github.com>
2024-08-02 01:44:17 +0700

    Update banner.py

--- script/banner.py
+++ script/banner.py
@@ -1,11 +1,19 @@
-
+from rich.console import Console
+#------------[ INDICATION ]---------------#
+M2 = "[#FF0000]" # MERAH
+H2 = "[#00FF00]" # HIJAU
+K2 = "[#FFFF00]" # KUNING
+B2 = "[#00C8FF]" # BIRU
+P2 = "[#FFFFFF]" # PUTIH
+U2 = "[#AF00FF]" # UNGU
+O2 = "[#FF8F00]" # ORANGE
 
 
 def title():
   Console().print(Panel("""
   [bold red]███████████████████████    
   [bold red]███████████████████████ [bold yellow]Github : [bold green]
-  [bold red]███████████████████████ [bold yellow]Wa     : [bold green]+62895386194***
+  [bold red]███████████████████████ [bold yellow]Wa     : [bold green]+628953596111**
   [bold white]███████████████████████          
   [bold white]███████████████████████          
   [bold white]███████████████████████ 
